88afbcbba282451a16206aeb82f67c421777f460
B-Pasqual <brunopasqual0@gmail.com>
2023-09-18 21:10:22 -0300

    exercicios da última aula

--- Algoritmo/aula06/exercicio1.py
+++ Algoritmo/aula06/exercicio1.py
@@ -1,10 +1,9 @@
-def ehDivisivel(numero):
+def ehDivisivel():
+    numero = float(input('Digite um número:'))
+    
     if numero % 3 == 0 and numero % 5 == 0:
         print('O numero foi divisivel por ambos')
     else:
         print('O numero nao foi divisivel')
         
-    
-    
-ehDivisivel(10)
-ehDivisivel(15)
+ehDivisivel()
--- Algoritmo/aula06/exercicio2.py
+++ Algoritmo/aula06/exercicio2.py
@@ -1,4 +1,6 @@
-def calculaDesconto(valorDaCompra):
+def calculaDesconto():
+    valorDaCompra = float(input('Digite o valor da sua compra'))
+    
     if valorDaCompra <= 1000:
         print(f'Valor do desconto: R$ {valorDaCompra* 0.1}')
     elif valorDaCompra > 1000 and valorDaCompra <= 5000:
@@ -6,4 +8,4 @@ def calculaDesconto(valorDaCompra):
     else:
         print(f'Valor do desconto: R$ {valorDaCompra*0.3}')
         
-calculaDesconto(5001)
+calculaDesconto()
